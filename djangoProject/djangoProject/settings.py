50523c162584c8caba448134ed8d18f7c613ea14
Hms200 <96504521+Hms200@users.noreply.github.com>
2022-07-01 13:46:30 +0900

    db 설정 변경

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -25,13 +24,7 @@ SECRET_KEY = 'django-insecure-&mb_d8lze$a9vt*ps*ca8f4oxo&7g)*yx26h!oim5$)(38)8ky
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.38.245.11',
-                 'elb-720373161.ap-northeast-2.elb.amazonaws.com',
-                 '172.31.11.235',
-                 '13.125.95.237',
-                 '172.31.7.57'
-                 ]
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -45,7 +38,8 @@ INSTALLED_APPS = [
     'order',
     'rest_framework',
     'boss',
-    'delivery'
+    'delivery',
+    'user'
 ]
 
 MIDDLEWARE = [
@@ -77,24 +71,27 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'project1',
-        'USER': 'admin',
-        'PASSWORD': 'munsuk4404',
-        'HOST': 'database-1.clv7g1lmh49f.ap-northeast-2.rds.amazonaws.com',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
+    # aws rds 사용 설정
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'project1',
+    #     'USER': 'admin',
+    #     'PASSWORD': 'munsuk4404',
+    #     'HOST': 'database-1.clv7g1lmh49f.ap-northeast-2.rds.amazonaws.com',
+    #     'PORT': '3306',
+    #     'OPTIONS': {
+    #         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+    #     }
+    # }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -114,7 +111,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -125,7 +121,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
